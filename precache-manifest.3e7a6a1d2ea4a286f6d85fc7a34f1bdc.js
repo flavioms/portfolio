19b1cc8c36c1501d0e2044cd4134c7e01df9a5db
unknown <flaviomartins_silva@hotmail.com>
2020-04-12 18:17:31 -0300

    Updates

---
+++ precache-manifest.3e7a6a1d2ea4a286f6d85fc7a34f1bdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455389496f83b77f2b76142c71fb6596",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f7eda977f7035f1b7c8b",
+    "url": "/portfolio/static/js/2.9b726f09.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/portfolio/static/js/2.9b726f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6922e538a15a7b050d6c",
+    "url": "/portfolio/static/js/main.13ae36a4.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
